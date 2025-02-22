707d4192233b28747c5ee81ffe53a9397d2cdb3c
eis <6283064@gmail.com>
2025-02-23 00:39:41 +0700

    Конструктор [#173889 #533558]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -5,7 +5,8 @@ public class Error {
     private int status;
     private String message;
 
-    public Error() {}
+    public Error() { }
+
     public Error(boolean active, int status, String message) {
         this.active = active;
         this.message = message;
