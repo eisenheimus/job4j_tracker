46c9eec8afe1607c7cd443da9262227efb4452f9
eis <6283064@gmail.com>
2025-03-01 00:07:40 +0700

    Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,20 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created;
 
-    public Item() { }
+    public Item() {
+        this.created = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
 
     public Item(String name) {
         this.name = name;
@@ -29,5 +39,15 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+}
+
+class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String date = (item.getCreated()).format(formatter);
+
+        System.out.println(date);
     }
 }
