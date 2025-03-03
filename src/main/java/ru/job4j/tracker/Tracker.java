6258683ee01584d03c5becd2a6aa7b2c9405543a
eis <6283064@gmail.com>
2025-03-03 11:37:41 +0700

    Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -43,6 +43,16 @@ public class Tracker {
         return true;
     }
 
+    public void delete(int id) {
+        int index = indexOf(id);
+
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+    }
+
     public Item[] findAll() {
         Item[] result = new Item[100];
         int size = 0;
