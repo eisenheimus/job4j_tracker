1ba5b6e6f273ffeffebf950232a8c296018bce6a
eis <6283064@gmail.com>
2025-03-04 00:22:48 +0700

    Tracker - хранилище [#396 #534553]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -58,28 +58,19 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] result = new Item[100];
-        int size = 0;
-        for (int i = 0; i < this.items.length; i++) {
-            if (this.items[i] != null) {
-                result[size] = this.items[i];
-                size++;
-            }
-        }
-
-        return Arrays.copyOf(result, size);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
         Item[] result = new Item[100];
-        int size = 0;
-        for (int i = 0; i < this.items.length; i++) {
-            if (this.items[i] != null && this.items[i].getName() == key) {
-                result[size] = this.items[i];
-                size++;
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (this.items[i].getName().equals(key)) {
+                result[count] = this.items[i];
+                count++;
             }
         }
 
-        return Arrays.copyOf(result, size);
+        return Arrays.copyOf(result, count);
     }
 }
